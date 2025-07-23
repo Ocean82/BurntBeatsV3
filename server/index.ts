53fdedade742b67cdba964f94ca7f39def53e275
Ocean82 <sammyjernigan@gmail.com>
2025-07-23 15:52:33 +0000

    issues cont

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fs from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = __filename || require.resolve("./index.js");
 const __dirname = dirname(__filename);
 
 // System operability check
